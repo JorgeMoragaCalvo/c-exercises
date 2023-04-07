ce83b43be6c9a4156c74c8fc85639493e81c6280
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2023-04-07 12:31:41 -0400

    Descriptions

--- removeElement/removeElement.c
+++ removeElement/removeElement.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/*From LeetCode 
+    The program return total number of elements not repeated
+*/
+
+
+/*FUNCTION PROTOTYPE*/
 int removeElement(int *, int, int);
 
+
+/*DRIVE CODE*/
 int main(){
     int nums[] = {3, 2, 2, 5, 6, 4};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
@@ -10,6 +18,8 @@ int main(){
     printf("%d\n", removeElement(nums, numsSize, val));
 }
 
+
+/*FUNCTION*/
 int removeElement(int *nums, int numsSize, int val){
     int count = 0, temp;
     for(int i = 0; i < numsSize; i++){
